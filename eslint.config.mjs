14e16b67db6c5ba0edca8c6b6481f7f69af48c1a
Sergey Astapov <sergeastapov@gmail.com>
2024-11-07 20:44:42 -0500

    sync with ember-cli config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,53 +1,132 @@
-// eslint.config.mjs
+/**
+ * Debugging:
+ *   https://eslint.org/docs/latest/use/configure/debug
+ *  ----------------------------------------------------
+ *
+ *   Print a file's calculated configuration
+ *
+ *     npx eslint --print-config path/to/file.js
+ *
+ *   Inspecting the config
+ *
+ *     npx eslint --inspect-config
+ *
+ */
+import globals from 'globals';
+import js from '@eslint/js';
 
-import { FlatCompat } from '@eslint/eslintrc';
-import emberPlugin from 'eslint-plugin-ember';
-import prettierPlugin from 'eslint-plugin-prettier';
-import js from '@eslint/js'; // Import ESLint core configs
-import globals from 'globals'; // Import globals
+import ember from 'eslint-plugin-ember/recommended';
+import prettier from 'eslint-plugin-prettier/recommended';
+import qunit from 'eslint-plugin-qunit';
+import n from 'eslint-plugin-n';
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.url,
-  resolvePluginsRelativeTo: import.meta.url,
-  recommendedConfig: js.configs.recommended, // Provide recommendedConfig
-});
+import babelParser from '@babel/eslint-parser';
+
+const esmParserOptions = {
+  ecmaFeatures: { modules: true },
+  ecmaVersion: 'latest',
+  requireConfigFile: false,
+  babelOptions: {
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+    ],
+  },
+};
 
 export default [
+  js.configs.recommended,
+  prettier,
+  ember.configs.base,
+  ember.configs.gjs,
+  /**
+   * Ignores must be in their own object
+   * https://eslint.org/docs/latest/use/configure/ignore
+   */
+  {
+    ignores: [
+      'blueprints/validator/files',
+      'dist/',
+      'node_modules/',
+      'coverage/',
+      '!**/.*',
+    ],
+  },
+  /**
+   * https://eslint.org/docs/latest/use/configure/configuration-files#configuring-linter-options
+   */
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'error',
+    },
+  },
   {
     files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module', // Ensure ES module parsing
+      parser: babelParser,
+    },
+  },
+  {
+    files: ['**/*.{js,gjs}'],
+    languageOptions: {
+      parserOptions: esmParserOptions,
       globals: {
         ...globals.browser,
       },
     },
-    plugins: {
-      ember: emberPlugin,
-      prettier: prettierPlugin,
-    },
-    rules: {},
   },
-  // Extend configurations using FlatCompat
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:ember/recommended',
-    'plugin:prettier/recommended',
-  ),
   {
-    ignores: [
-      'node_modules/',
-      'dist/',
-      'blueprints',
-      '.prettierrc.cjs',
-      '.template-lintrc.js',
-      'config/ember-try.js',
-      'ember-cli-build.js',
+    files: ['tests/**/*-test.{js,gjs}'],
+    plugins: {
+      qunit,
+    },
+  },
+  /**
+   * CJS node files
+   */
+  {
+    files: [
+      '**/*.cjs',
+      'blueprints/**/*.js',
+      'config/**/*.js',
+      'tests/dummy/config/**/*.js',
+      'node-test/**/*.js',
       'index.js',
-      'node-test/',
-      'config/environment.js',
-      'tests/dummy/',
       'testem.js',
+      'testem*.js',
+      '.prettierrc.js',
+      '.stylelintrc.js',
+      '.template-lintrc.js',
+      'ember-cli-build.js',
     ],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'script',
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.mocha,
+        ...globals.node,
+      },
+    },
+  },
+  /**
+   * ESM node files
+   */
+  {
+    files: ['**/*.mjs'],
+    plugins: {
+      n,
+    },
+
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      parserOptions: esmParserOptions,
+      globals: {
+        ...globals.node,
+      },
+    },
   },
 ];
